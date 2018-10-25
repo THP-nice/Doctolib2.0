37f4388accc49e178fcdd2132d1accf529bcd501
tutus06 <arthurmansuy@hotmail.fr>
2018-10-25 18:52:27 +0200

    final commit

--- Doctolib/db/schema.rb
+++ Doctolib/db/schema.rb
@@ -10,23 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_25_123314) do
+ActiveRecord::Schema.define(version: 2018_10_25_161433) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "date"
     t.integer "doctor_id"
     t.integer "patient_id"
+    t.integer "city_id"
+    t.index ["city_id"], name: "index_appointments_on_city_id"
     t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
     t.index ["patient_id"], name: "index_appointments_on_patient_id"
+  end
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "doctors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "speciality"
     t.integer "postal_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "city_id"
+    t.index ["city_id"], name: "index_doctors_on_city_id"
+  end
+
+  create_table "doctors_specialities", id: false, force: :cascade do |t|
+    t.integer "doctor_id"
+    t.integer "speciality_id"
+    t.index ["doctor_id"], name: "index_doctors_specialities_on_doctor_id"
+    t.index ["speciality_id"], name: "index_doctors_specialities_on_speciality_id"
   end
 
   create_table "patients", force: :cascade do |t|
@@ -34,6 +50,16 @@ ActiveRecord::Schema.define(version: 2018_10_25_123314) do
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "city_id"
+    t.index ["city_id"], name: "index_patients_on_city_id"
+  end
+
+  create_table "specialities", force: :cascade do |t|
+    t.string "name"
+    t.integer "doctor_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["doctor_id"], name: "index_specialities_on_doctor_id"
   end
 
 end
